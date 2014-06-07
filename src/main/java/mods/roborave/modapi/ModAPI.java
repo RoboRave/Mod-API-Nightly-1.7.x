a48f29b24d0ce361e3b6b0c855b6a4c1baac8e6e
RoboRave <zdowding@gmail.com>
2014-06-06 23:30:03 -0600

    fisrt commit basic files

---
+++ src/main/java/mods/roborave/modapi/ModAPI.java
@@ -0,0 +1,56 @@
+package mods.roborave.modapi;
+
+import java.io.File;
+import java.security.cert.Certificate;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.eventbus.EventBus;
+
+import mods.roborave.modapi.lib.Strings;
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.LoadController;
+import cpw.mods.fml.common.MetadataCollection;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.ModContainer;
+import cpw.mods.fml.common.ModMetadata;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.versioning.ArtifactVersion;
+import cpw.mods.fml.common.versioning.VersionRange;
+
+@Mod(modid = ModAPI.MODID, version = ModAPI.VERSION,name="Mod-API-Nly(B2XQVsAJGjqZ9en)")
+public class ModAPI extends DummyModContainer
+{
+    public static final String MODID = "Mod-API-Nightly";
+    public static final String VERSION = Strings.buildType+"-0.0.1-"+ Strings.getHash + Strings.getBuild + Strings.getTimeStamp;
+    
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    }
+   
+    
+    
+	@Override
+	public String getModId() 
+	{
+		return null;
+	}
+	
+	@Override
+	public String getName() 
+	{
+		return null;
+	}
+	
+	@Override
+	public String getVersion() 
+	{
+		return VERSION;
+	}
+}
---
+++ src/main/java/mods/roborave/modapi/lib/Strings.java
@@ -0,0 +1,10 @@
+package mods.roborave.modapi.lib;
+
+public class Strings {
+
+	public static final String buildType="pre-alpha";
+	public static final String getBuild="Build 1";
+	public static final String getHash= "T3uRCKKmVcJiBfX";
+	public static final String getTimeStamp="Timestamp: 2014-06-07 05:26:21 UTC";
+	
+}
